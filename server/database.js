368c533819b75fd9c6d5fca5d1a1ac95c4d68657
Saoussen Slii <38138018+sawsanCS@users.noreply.github.com>
2021-09-08 10:38:45 -0400

    changed the url to connect the db

--- server/database.js
+++ server/database.js
@@ -1,6 +1,7 @@
 const MongoClient = require("mongodb").MongoClient;
 //Create a database named "mydb":
-const url = "mongodb://localhost:27017/mydb";
+
+const url = `mongodb+srv://saoussen:%40vera9e135@cluster0.507fy.mongodb.net/test`;
 
 MongoClient.connect(url, function (err, db) {
   if (err) throw err;
