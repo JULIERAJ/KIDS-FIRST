d6dfc6c4c73bcdbafbf54f9d6dfb6997b48e159a
Alona Klymenko <alona.klymenko17@gmail.com>
2021-12-15 13:01:52 -0500

    rm ip address print

--- server/app.js
+++ server/app.js
@@ -7,26 +7,6 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const env = require("dotenv");
 
-const { networkInterfaces } = require('os');
-
-const nets = networkInterfaces();
-const results = Object.create(null); // Or just '{}', an empty object
-
-for (const name of Object.keys(nets)) {
-    for (const net of nets[name]) {
-        // Skip over non-IPv4 and internal (i.e. 127.0.0.1) addresses
-        if (net.family === 'IPv4' && !net.internal) {
-            if (!results[name]) {
-                results[name] = [];
-            }
-            results[name].push(net.address);
-        }
-    }
-}
-console.log(JSON.stringify(results, null, 4));
-
-env.config()
-
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 
